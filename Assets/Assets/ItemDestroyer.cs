68312a58f892fb6aae46185bf35c589499091a51
aoki <vamos23467@gmail.com>
2018-06-20 14:37:42 +0900

    StepAsideUnitychan
    First Commit for this project

---
+++ Assets/Assets/ItemDestroyer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDestroyer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	//画面外のオブジェクトを削除する
+	void OnBecameInvisible()
+	{
+		Destroy(this.gameObject);
+	}
+}
